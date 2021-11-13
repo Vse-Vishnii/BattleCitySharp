2f68721d86b354c17c531a2dcd232ed528cdd2a6
Vse-Vishnii <Lesha37b@yandex.ru>
2021-11-13 11:31:41 +0500

    Rename

--- PlatformerWPF/App.xaml.cs
+++ PlatformerWPF/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace PlatformerWPF
+namespace BattleCitySharp
 {
     /// <summary>
     /// Interaction logic for App.xaml
