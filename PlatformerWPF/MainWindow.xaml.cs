6c1fd8525049a11f47fb1a6d7b4c4bd51e7ac648
PendosNaRakete <sakancev66@gmail.com>
2021-11-25 12:22:19 +0500

    add disign

--- PlatformerWPF/MainWindow.xaml.cs
+++ PlatformerWPF/MainWindow.xaml.cs
@@ -1,28 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace BattleCitySharp
+namespace WpfApplication1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private Random random = new Random();
+        private int offset = 0;
+        
+        private Uri[] wallType = new Uri[]
+        {
+            new Uri("pack://application:,,,/Resourses/wall1.png"),
+            new Uri("pack://application:,,,/Resourses/wall2.png"),
+            new Uri("pack://application:,,,/Resourses/wall3.png"),
+            new Uri("pack://application:,,,/Resourses/wall4.png")
+        };
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var wallIndex = random.Next(wallType.Length);
+            Wall1.Source = BitmapFrame.Create(wallType[wallIndex]);
+            offset++;
+            Canvas.SetLeft(Tank1, offset);
+            //Canvas1.UpdateLayout();
         }
     }
 }
