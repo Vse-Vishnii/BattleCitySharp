cf966262ed85d45c7d05433fb8ba680075ef53cf
Vse-Vishnii <Lesha37b@yandex.ru>
2021-12-07 12:40:14 +0500

    Восстановил

---
+++ PlatformerWPF/Domain/Components/MonoBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BattleCitySharp
+{
+    public abstract class MonoBehavior
+    {
+        protected Transform Transform { get; set; }
+
+        public virtual void Update() { }
+        public virtual void LateUpdate() { }
+        public virtual void ColliderEnter(Collider collider) { }
+        public virtual void ColliderStay(Collider collider) { }
+        public virtual void ColliderExit(Collider collider) { }
+
+        protected object Instantiate(object original, Point position, Quaternion rotation)
+        {
+            Runner.objects.Add((MonoBehavior)original);
+
+
+
+            return original;
+        }
+    }
+}
---
+++ PlatformerWPF/Domain/Utility/Time.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleCitySharp
+{
+    public class Time
+    {
+        public static float DeltaTime 
+        { 
+            get
+            {
+                return 0;
+            } 
+        }
+    }
+}
