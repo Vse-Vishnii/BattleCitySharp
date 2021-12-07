ea0a37866055e4851f05e831208e5f2009b7414a
Vse-Vishnii <Lesha37b@yandex.ru>
2021-12-07 12:24:21 +0500

    MainWindowChange

--- PlatformerWPF/MainWindow.xaml.cs
+++ PlatformerWPF/MainWindow.xaml.cs
@@ -20,46 +20,68 @@ namespace BattleCitySharp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
-        private double x = 0;
-        private int frames = 0;
-        private Timer timer;
-        Stopwatch watch = new Stopwatch();
+        private Random random = new Random();
+        private int offset = 0;
+
+        private Uri[] wallType = new Uri[]
+        {
+            new Uri("pack://application:,,,/Resourses/wall1.png"),
+            new Uri("pack://application:,,,/Resourses/wall2.png"),
+            new Uri("pack://application:,,,/Resourses/wall3.png"),
+            new Uri("pack://application:,,,/Resourses/wall4.png")
+        };
+
         public MainWindow()
         {
             InitializeComponent();
-            timer = new Timer(10);
-            timer.Enabled = true;            
-            watch.Start();
-            timer.Elapsed += new ElapsedEventHandler(OnTimeEvent);
         }
 
-        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        public void Button_Click(object sender, RoutedEventArgs e)
         {
-            frames++;
-            var a = 0;
-            if (watch.ElapsedMilliseconds >= 1000)
+            /*var wallIndex = random.Next(wallType.Length);
+            Wall1.Source = BitmapFrame.Create(wallType[wallIndex]);
+            offset++;
+            Canvas.SetLeft(Tank1, offset);*/
+            //Canvas1.UpdateLayout();
+
+            //Инициализация объекта (Дефолтные размеры и смещения, иначе они будут NaN)
+            var tank1 = new Image();
+            tank1.Width = 70;
+            tank1.Height = 70;
+            Canvas.SetLeft(tank1, 1);
+            Canvas.SetTop(tank1, 0);
+
+            tank1.Source = BitmapFrame.Create(new Uri("pack://application:,,,/Resourses/tank1.png"));
+
+            //Поворот объекта
+            RotateTransform rotateTransform1 = new RotateTransform(0);
+
+            //Центр вращения
+            rotateTransform1.CenterX = 35;
+            rotateTransform1.CenterY = 35;
+            tank1.RenderTransform = rotateTransform1;
+
+            //Добавление объекта на холст
+            Canvas1.Children.Add(tank1);
+
+            //Назовем их колаайдерами
+            var rect1 = new Rect(Canvas.GetLeft(tank1), Canvas.GetTop(tank1), tank1.Width, tank1.Height);
+            var rect2 = new Rect(Canvas.GetLeft(Wall1), Canvas.GetTop(Wall1), Wall1.Width, Wall1.Height);
+
+            //Проверка на коллижн
+            if (rect1.IntersectsWith(rect2))
             {
-                watch.Stop();
-                a = frames;
+                //Аналогичные шаги как с tank1
+                var tank2 = new Image();
+                tank2.Width = 70;
+                tank2.Height = 70;
+                Canvas.SetLeft(tank2, 0);
+                Canvas.SetTop(tank2, 210);
+                tank2.Source = BitmapFrame.Create(new Uri("pack://application:,,,/Resourses/tank1.png"));
+                Canvas1.Children.Add(tank2);
             }
-                
-            this.Dispatcher.Invoke(() =>
-            {
-                x ++;
-                Canvas.SetLeft(tank, x);
-            });            
-        }
-
-        static long lastTime = 0;
-        static double GetDeltaTime()
-        {
-            long now = DateTime.Now.Millisecond;
-            double dT = (now - lastTime) / 1000; // / 1000
-            lastTime = now;
-            Console.WriteLine(dT);
-            return dT;
         }
     }
 }
